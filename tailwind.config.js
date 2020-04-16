3eb151767030a1dbb3691dd43c35443496a0b01c
Ratko Solaja <solaja.ratko@gmail.com>
2020-04-16 14:51:44 +0200

    Finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,21 +10,56 @@ module.exports = {
             'xl': '1280px',
         },
         extend: {
+            colors: {
+                theme: {
+                    'yellow': '#FCD192',
+                    'yellow-darker': '#FFC268',
+                    'red': '#c1272d'
+                }
+            },
             container: {
                 center: true,
-                //padding: '1.25rem'
+                padding: '4.375rem'
             },
             spacing: {
                 '7': '1.75rem',
                 '9': '2.25rem',
-                '11': '2.75rem'
+                '11': '2.75rem',
+                '15': '3.75rem',
+                '18': '4.5rem',
+                '21-1/2': '5.375rem',
+                '28': '7rem',
+                '45': '11.25rem'
             },
             fontSize: {
                 'ms': '0.8125rem',
-                'm': '0.9375rem'
+                'm': '0.9375rem',
+                '32': '2rem',
+                '40': '2.5rem',
+                '60': '3.75rem',
+                '70': '4.375rem',
+                '76': '4.75rem',
+                '113': '7.0625rem',
+                '124': '7.75rem'
+            },
+            borderRadius: {
+                '20': '1.25rem'
+            },
+            fontFamily: {
+                sans: [
+                    'Open Sans',
+                    ...defaultTheme.fontFamily.sans
+                ],
+                serif: [
+                    'MonstroSolid',
+                    ...defaultTheme.fontFamily.serif
+                ]
             }
         }
     },
-    variants: {},
+    variants: {
+        scale: ['responsive', 'hover', 'focus', 'group-hover'],
+        opacity: ['responsive', 'hover', 'focus', 'group-hover']
+    },
     plugins: []
 };
